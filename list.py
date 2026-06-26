66be8dd3a4f56d88b9578232c3fe91414ee5dcb7
THAKOR MAHESH KUMAR <maheshthakor9315@gmail.com>
2026-06-26 15:16:56 +0530

    list.demo

---
+++ list.py
@@ -0,0 +1,16 @@
+      
+
+     #my list
+
+mylist = []
+count = int(input("hoew many record ?"))
+for i in range(count):
+    value = input("enter value:")
+    mylist.append(value)
+    print(mylist)
+
+
+
+    
+
+
